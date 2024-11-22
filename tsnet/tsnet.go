7c5f6b5e2aacb0b5baf61f74f7e8994b0a6b1b9d
世界 <i@sekai.icu>
2024-11-22 22:35:16 +0800

    Remove set package

--- tsnet/tsnet.go
+++ tsnet/tsnet.go
@@ -34,7 +34,6 @@ import (
 	_ "github.com/sagernet/tailscale/feature/condregister/portmapper"
 	_ "github.com/sagernet/tailscale/feature/condregister/useproxy"
 	"github.com/sagernet/tailscale/health"
-	"github.com/sagernet/tailscale/hostinfo"
 	"github.com/sagernet/tailscale/internal/client/tailscale"
 	"github.com/sagernet/tailscale/ipn"
 	"github.com/sagernet/tailscale/ipn/ipnauth"
@@ -337,7 +336,6 @@ func (h *localSecHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // Start connects the server to the tailnet.
 // Optional: any calls to Dial/Listen will also call Start.
 func (s *Server) Start() error {
-	hostinfo.SetPackage("tsnet")
 	s.initOnce.Do(s.doInit)
 	return s.initErr
 }
