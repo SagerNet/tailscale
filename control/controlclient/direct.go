2e099df1721da46932d17a162d3e0598d8dbf764
世界 <i@sekai.icu>
2025-02-12 18:30:58 +0800

    Export DNS resolver

--- control/controlclient/direct.go
+++ control/controlclient/direct.go
@@ -162,6 +162,8 @@ type Options struct {
 	// attempted. It is used to allow the client to clean up any resources or complete any
 	// tasks that are dependent on a live client.
 	Shutdown func()
+
+	LookupHook dnscache.LookupHookFunc
 }
 
 // ControlDialPlanner is the interface optionally supplied when creating a
@@ -253,6 +255,7 @@ func NewDirect(opts Options) (*Direct, error) {
 		UseLastGood:      true,
 		LookupIPFallback: dnsfallback.MakeLookupFunc(opts.Logf, netMon),
 		Logf:             opts.Logf,
+		LookupHook:       opts.LookupHook,
 	}
 
 	httpc := opts.HTTPTestClient
