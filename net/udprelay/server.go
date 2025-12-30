b5ac99cdf598578f61aceb020a6c265d83c1329d
世界 <i@sekai.icu>
2025-12-30 22:19:02 +0800

    Fix udprelay

--- net/udprelay/server.go
+++ net/udprelay/server.go
@@ -328,7 +328,7 @@ func NewServer(logf logger.Logf, port uint16, onlyStaticAddrPorts bool) (s *Serv
 	// in a running client.
 	bus := eventbus.New()
 	s.bus = bus
-	netMon, err := netmon.New(s.bus, logf)
+	netMon, err := netmon.New(s.bus, logf, nil)
 	if err != nil {
 		return nil, err
 	}
